b07264e0a382e273dad792b267f13aa1c30bb2bf
mmhk30313 <mmhk.mmhkitsforyou@gmail.com>
2021-12-27 01:26:25 +0600

    Phone book app is updated

--- client/src/services/utils/server.js
+++ client/src/services/utils/server.js
@@ -1,3 +1,3 @@
 export const baseUrl = 'http://localhost:4000';
-export const liveUrl = null;
-// export const liveUrl = "https://phone-book.herokuapp.com";
+// export const liveUrl = null;
+export const liveUrl = "https://mmhk-phone-book.herokuapp.com";
